b5588535d8a0bb90f9259ad4709ac5ae9cf65829
Kistlak <kistlakall@gmail.com>
2018-04-24 02:20:24 +0530

    Basic Spring Boot

---
+++ src/main/java/org/kisalka/cw/apicw/ApicwApplication.java
@@ -0,0 +1,12 @@
+package org.kisalka.cw.apicw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicwApplication.class, args);
+	}
+}
---
+++ src/test/java/org/kisalka/cw/apicw/ApicwApplicationTests.java
@@ -0,0 +1,16 @@
+package org.kisalka.cw.apicw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApicwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
